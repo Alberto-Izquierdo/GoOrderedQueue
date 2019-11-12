b8e16e49201cb5d07bc4e337d1ac84d8457c8bd9
Alberto-Izquierdo <alberto.izquierdo.ramirez@gmail.com>
2019-11-12 19:36:53 +0100

    Added support for any basic type

--- OrderedQueue.go
+++ OrderedQueue.go
@@ -1,26 +1,142 @@
 package ordered_queue
 
-import "errors"
+import (
+	"errors"
+	"reflect"
+)
 
 type QueueElement interface {
 	LessThan(QueueElement) bool
 }
 
 type OrderedQueue struct {
-	elements []QueueElement
+	elements []interface{}
 }
 
-func (q *OrderedQueue) Push(newElement QueueElement) {
-	for index, value := range q.elements {
-		if newElement.LessThan(value) {
-			q.elements = append(q.elements[:index], append([]QueueElement{newElement}, q.elements[index:]...)...)
-			return
+func (q *OrderedQueue) Push(newElement interface{}) (err error) {
+	if q.Size() > 0 {
+		if reflect.TypeOf(q.elements[0]) != reflect.TypeOf(newElement) {
+			return errors.New("Trying to push an element of a different type from the original")
 		}
 	}
-	q.elements = append(q.elements, newElement)
+	switch newElementCasted := newElement.(type) {
+	case int:
+		for index, value := range q.elements {
+			if newElementCasted < value.(int) {
+				q.elements = append(q.elements[:index], append([]interface{}{newElement}, q.elements[index:]...)...)
+				return nil
+			}
+		}
+		q.elements = append(q.elements, newElementCasted)
+	case int8:
+		for index, value := range q.elements {
+			if newElementCasted < value.(int8) {
+				q.elements = append(q.elements[:index], append([]interface{}{newElement}, q.elements[index:]...)...)
+				return nil
+			}
+		}
+		q.elements = append(q.elements, newElementCasted)
+	case int16:
+		for index, value := range q.elements {
+			if newElementCasted < value.(int16) {
+				q.elements = append(q.elements[:index], append([]interface{}{newElement}, q.elements[index:]...)...)
+				return nil
+			}
+		}
+		q.elements = append(q.elements, newElementCasted)
+	case int32:
+		for index, value := range q.elements {
+			if newElementCasted < value.(int32) {
+				q.elements = append(q.elements[:index], append([]interface{}{newElement}, q.elements[index:]...)...)
+				return nil
+			}
+		}
+		q.elements = append(q.elements, newElementCasted)
+	case int64:
+		for index, value := range q.elements {
+			if newElementCasted < value.(int64) {
+				q.elements = append(q.elements[:index], append([]interface{}{newElement}, q.elements[index:]...)...)
+				return nil
+			}
+		}
+		q.elements = append(q.elements, newElementCasted)
+	case uint:
+		for index, value := range q.elements {
+			if newElementCasted < value.(uint) {
+				q.elements = append(q.elements[:index], append([]interface{}{newElement}, q.elements[index:]...)...)
+				return nil
+			}
+		}
+		q.elements = append(q.elements, newElementCasted)
+	case uint8:
+		for index, value := range q.elements {
+			if newElementCasted < value.(uint8) {
+				q.elements = append(q.elements[:index], append([]interface{}{newElement}, q.elements[index:]...)...)
+				return nil
+			}
+		}
+		q.elements = append(q.elements, newElementCasted)
+	case uint16:
+		for index, value := range q.elements {
+			if newElementCasted < value.(uint16) {
+				q.elements = append(q.elements[:index], append([]interface{}{newElement}, q.elements[index:]...)...)
+				return nil
+			}
+		}
+		q.elements = append(q.elements, newElementCasted)
+	case uint32:
+		for index, value := range q.elements {
+			if newElementCasted < value.(uint32) {
+				q.elements = append(q.elements[:index], append([]interface{}{newElement}, q.elements[index:]...)...)
+				return nil
+			}
+		}
+		q.elements = append(q.elements, newElementCasted)
+	case uint64:
+		for index, value := range q.elements {
+			if newElementCasted < value.(uint64) {
+				q.elements = append(q.elements[:index], append([]interface{}{newElement}, q.elements[index:]...)...)
+				return nil
+			}
+		}
+		q.elements = append(q.elements, newElementCasted)
+	case float32:
+		for index, value := range q.elements {
+			if newElementCasted < value.(float32) {
+				q.elements = append(q.elements[:index], append([]interface{}{newElement}, q.elements[index:]...)...)
+				return nil
+			}
+		}
+		q.elements = append(q.elements, newElementCasted)
+	case float64:
+		for index, value := range q.elements {
+			if newElementCasted < value.(float64) {
+				q.elements = append(q.elements[:index], append([]interface{}{newElement}, q.elements[index:]...)...)
+				return nil
+			}
+		}
+		q.elements = append(q.elements, newElementCasted)
+	case string:
+		for index, value := range q.elements {
+			if newElementCasted < value.(string) {
+				q.elements = append(q.elements[:index], append([]interface{}{newElement}, q.elements[index:]...)...)
+				return nil
+			}
+		}
+		q.elements = append(q.elements, newElementCasted)
+	case QueueElement:
+		for index, value := range q.elements {
+			if newElementCasted.LessThan(value.(QueueElement)) {
+				q.elements = append(q.elements[:index], append([]interface{}{newElementCasted}, q.elements[index:]...)...)
+				return nil
+			}
+		}
+		q.elements = append(q.elements, newElementCasted)
+	}
+	return nil
 }
 
-func (q *OrderedQueue) Pop() (element QueueElement, err error) {
+func (q *OrderedQueue) Pop() (element interface{}, err error) {
 	if q.Size() > 0 {
 		element = q.elements[0]
 		q.elements = q.elements[1:]
--- OrderedQueue_test.go
+++ OrderedQueue_test.go
@@ -4,6 +4,74 @@ import (
 	"testing"
 )
 
+func TestQueueWithInts(t *testing.T) {
+	q := OrderedQueue{}
+	if err := q.Push(3); err != nil {
+		t.Errorf("Pushing correct values should not return an error")
+	}
+	if err := q.Push(7); err != nil {
+		t.Errorf("Pushing correct values should not return an error")
+	}
+	if err := q.Push(1); err != nil {
+		t.Errorf("Pushing correct values should not return an error")
+	}
+	if q.Size() != 3 {
+		t.Errorf("The queue should contain 3 elements")
+	}
+	if value, err := q.Pop(); err != nil {
+		t.Errorf("Value should not be nil")
+	} else if castedValue := value.(int); castedValue != 1 {
+		t.Errorf("First value should be 1, instead it is %d", castedValue)
+	}
+	if value, err := q.Pop(); err != nil {
+		t.Errorf("Value should not be nil")
+	} else if castedValue := value.(int); castedValue != 3 {
+		t.Errorf("First value should be 3, instead it is %d", castedValue)
+	}
+	if value, err := q.Pop(); err != nil {
+		t.Errorf("Value should not be nil")
+	} else if castedValue := value.(int); castedValue != 7 {
+		t.Errorf("First value should be 7, instead it is %d", castedValue)
+	}
+	if _, err := q.Pop(); err == nil {
+		t.Errorf("Pop() on empty queue should return an error")
+	}
+}
+
+func TestQueueWithStrings(t *testing.T) {
+	q := OrderedQueue{}
+	if err := q.Push("asdf"); err != nil {
+		t.Errorf("Pushing correct values should not return an error")
+	}
+	if err := q.Push("zxcv"); err != nil {
+		t.Errorf("Pushing correct values should not return an error")
+	}
+	if err := q.Push("qwer"); err != nil {
+		t.Errorf("Pushing correct values should not return an error")
+	}
+	if q.Size() != 3 {
+		t.Errorf("The queue should contain 3 elements")
+	}
+	if value, err := q.Pop(); err != nil {
+		t.Errorf("Value should not be nil")
+	} else if castedValue := value.(string); castedValue != "asdf" {
+		t.Errorf("First value should be asdf, instead it is %s", castedValue)
+	}
+	if value, err := q.Pop(); err != nil {
+		t.Errorf("Value should not be nil")
+	} else if castedValue := value.(string); castedValue != "qwer" {
+		t.Errorf("First value should be qwer, instead it is %s", castedValue)
+	}
+	if value, err := q.Pop(); err != nil {
+		t.Errorf("Value should not be nil")
+	} else if castedValue := value.(string); castedValue != "zxcv" {
+		t.Errorf("First value should be zxcv, instead it is %s", castedValue)
+	}
+	if _, err := q.Pop(); err == nil {
+		t.Errorf("Pop() on empty queue should return an error")
+	}
+}
+
 type MyData struct {
 	A int
 	B string
@@ -16,11 +84,17 @@ func (this MyData) LessThan(other QueueElement) bool {
 	return false
 }
 
-func TestLoadConfigurationFromPathInvalidPath(t *testing.T) {
+func TestQueueWithCustomStruct(t *testing.T) {
 	q := OrderedQueue{}
-	q.Push(MyData{3, "asdf"})
-	q.Push(MyData{7, "qwer"})
-	q.Push(MyData{1, "zxcv"})
+	if err := q.Push(MyData{3, "asdf"}); err != nil {
+		t.Errorf("Pushing correct values should not return an error")
+	}
+	if err := q.Push(MyData{7, "qwer"}); err != nil {
+		t.Errorf("Pushing correct values should not return an error")
+	}
+	if err := q.Push(MyData{1, "zxcv"}); err != nil {
+		t.Errorf("Pushing correct values should not return an error")
+	}
 	if q.Size() != 3 {
 		t.Errorf("The queue should contain 3 elements")
 	}
@@ -47,5 +121,18 @@ func TestLoadConfigurationFromPathInvalidPath(t *testing.T) {
 	}
 	if _, err := q.Pop(); err == nil {
 		t.Errorf("Pop() on empty queue should return an error")
+	}
+}
+
+func TestQueueWithWrongValues(t *testing.T) {
+	q := OrderedQueue{}
+	if err := q.Push(3); err != nil {
+		t.Errorf("Pushing correct values should not return an error")
+	}
+	if err := q.Push("asdf"); err == nil {
+		t.Errorf("Pushing wrong values should return an error")
+	}
+	if q.Size() != 1 {
+		t.Errorf("The queue should contain 1 elements")
 	}
 }
