d2cd445bf709c4d3bbd11cac45d48e3d805400d5
Alberto-Izquierdo <alberto.izquierdo.ramirez@gmail.com>
2020-02-12 19:45:36 +0100

    Testify framework used for testing

--- OrderedQueue_test.go
+++ OrderedQueue_test.go
@@ -2,40 +2,27 @@ package ordered_queue
 
 import (
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestQueueWithInts(t *testing.T) {
 	q := OrderedQueue{}
-	if err := q.Push(3); err != nil {
-		t.Errorf("Pushing correct values should not return an error")
-	}
-	if err := q.Push(7); err != nil {
-		t.Errorf("Pushing correct values should not return an error")
-	}
-	if err := q.Push(1); err != nil {
-		t.Errorf("Pushing correct values should not return an error")
-	}
-	if q.Size() != 3 {
-		t.Errorf("The queue should contain 3 elements")
-	}
-	if value, err := q.Pop(); err != nil {
-		t.Errorf("Value should not be nil")
-	} else if castedValue := value.(int); castedValue != 1 {
-		t.Errorf("First value should be 1, instead it is %d", castedValue)
-	}
-	if value, err := q.Pop(); err != nil {
-		t.Errorf("Value should not be nil")
-	} else if castedValue := value.(int); castedValue != 3 {
-		t.Errorf("First value should be 3, instead it is %d", castedValue)
-	}
-	if value, err := q.Pop(); err != nil {
-		t.Errorf("Value should not be nil")
-	} else if castedValue := value.(int); castedValue != 7 {
-		t.Errorf("First value should be 7, instead it is %d", castedValue)
-	}
-	if _, err := q.Pop(); err == nil {
-		t.Errorf("Pop() on empty queue should return an error")
-	}
+	assert.Nil(t, q.Push(3), "Pushing correct values should not return an error")
+	assert.Nil(t, q.Push(7), "Pushing correct values should not return an error")
+	assert.Nil(t, q.Push(1), "Pushing correct values should not return an error")
+	assert.Equal(t, 3, q.Size(), "The queue should contain 3 elements")
+	value, err := q.Pop()
+	assert.Nil(t, err, "Error should be nil when poping elements from queue with elements")
+	assert.Equal(t, 1, value.(int), "First value should be 1, instead it is %d", value.(int))
+	value, err = q.Pop()
+	assert.Nil(t, err, "Error should be nil when poping elements from queue with elements")
+	assert.Equal(t, 3, value.(int), "First value should be 3, instead it is %d", value.(int))
+	value, err = q.Pop()
+	assert.Nil(t, err, "Error should be nil when poping elements from queue with elements")
+	assert.Equal(t, 7, value.(int), "First value should be 7, instead it is %d", value.(int))
+	value, err = q.Pop()
+	assert.NotNil(t, err, "Pop() on empty queue should return an error")
 }
 
 type NewStruct struct {
@@ -45,44 +32,26 @@ type NewStruct struct {
 func TestQueueWithWrongStruct(t *testing.T) {
 	q := OrderedQueue{}
 	var value NewStruct
-	err := q.Push(value)
-	if err == nil {
-		t.Errorf("Pushing wrong struct should return an error")
-	}
+	assert.NotNil(t, q.Push(value), "Pushing a struct that do not implement the \"QueueElement\" should return an error")
 }
 
 func TestQueueWithStrings(t *testing.T) {
 	q := OrderedQueue{}
-	if err := q.Push("asdf"); err != nil {
-		t.Errorf("Pushing correct values should not return an error")
-	}
-	if err := q.Push("zxcv"); err != nil {
-		t.Errorf("Pushing correct values should not return an error")
-	}
-	if err := q.Push("qwer"); err != nil {
-		t.Errorf("Pushing correct values should not return an error")
-	}
-	if q.Size() != 3 {
-		t.Errorf("The queue should contain 3 elements")
-	}
-	if value, err := q.Pop(); err != nil {
-		t.Errorf("Value should not be nil")
-	} else if castedValue := value.(string); castedValue != "asdf" {
-		t.Errorf("First value should be asdf, instead it is %s", castedValue)
-	}
-	if value, err := q.Pop(); err != nil {
-		t.Errorf("Value should not be nil")
-	} else if castedValue := value.(string); castedValue != "qwer" {
-		t.Errorf("First value should be qwer, instead it is %s", castedValue)
-	}
-	if value, err := q.Pop(); err != nil {
-		t.Errorf("Value should not be nil")
-	} else if castedValue := value.(string); castedValue != "zxcv" {
-		t.Errorf("First value should be zxcv, instead it is %s", castedValue)
-	}
-	if _, err := q.Pop(); err == nil {
-		t.Errorf("Pop() on empty queue should return an error")
-	}
+	assert.Nil(t, q.Push("asdf"), "Pushing correct values should not return an error")
+	assert.Nil(t, q.Push("zxcv"), "Pushing correct values should not return an error")
+	assert.Nil(t, q.Push("qwer"), "Pushing correct values should not return an error")
+	assert.Equal(t, 3, q.Size(), "The queue should contain 3 elements")
+	value, err := q.Pop()
+	assert.Nil(t, err, "Error should be nil when poping elements from queue with elements")
+	assert.Equal(t, "asdf", value.(string), "First value should be \"asdf\", instead it is %s", value.(string))
+	value, err = q.Pop()
+	assert.Nil(t, err, "Error should be nil when poping elements from queue with elements")
+	assert.Equal(t, "qwer", value.(string), "First value should be \"qwer\", instead it is %s", value.(string))
+	value, err = q.Pop()
+	assert.Nil(t, err, "Error should be nil when poping elements from queue with elements")
+	assert.Equal(t, "zxcv", value.(string), "First value should be \"zxcv\", instead it is %s", value.(string))
+	value, err = q.Pop()
+	assert.NotNil(t, err, "Pop() on empty queue should return an error")
 }
 
 type MyData struct {
@@ -96,55 +65,31 @@ func (this MyData) LessThan(other interface{}) bool {
 
 func TestQueueWithCustomStruct(t *testing.T) {
 	q := OrderedQueue{}
-	if err := q.Push(MyData{3, "asdf"}); err != nil {
-		t.Errorf("Pushing correct values should not return an error")
-	}
-	if err := q.Push(MyData{7, "qwer"}); err != nil {
-		t.Errorf("Pushing correct values should not return an error")
-	}
-	if err := q.Push(MyData{1, "zxcv"}); err != nil {
-		t.Errorf("Pushing correct values should not return an error")
-	}
-	if q.Size() != 3 {
-		t.Errorf("The queue should contain 3 elements")
-	}
-	if value, err := q.Pop(); err != nil {
-		t.Errorf("Value should not be nil")
-	} else if castedValue := value.(MyData); castedValue.A != 1 {
-		t.Errorf("Value A should be 1, instead it is %d", castedValue.A)
-	} else if castedValue.B != "zxcv" {
-		t.Errorf("Value A should be zxcv, instead it is %s", castedValue.B)
-	}
-	if value, err := q.Pop(); err != nil {
-		t.Errorf("Value should not be nil")
-	} else if castedValue := value.(MyData); castedValue.A != 3 {
-		t.Errorf("Value A should be 3, instead it is %d", castedValue.A)
-	} else if castedValue.B != "asdf" {
-		t.Errorf("Value A should be asdf, instead it is %s", castedValue.B)
-	}
-	if value, err := q.Pop(); err != nil {
-		t.Errorf("Value should not be nil")
-	} else if castedValue := value.(MyData); castedValue.A != 7 {
-		t.Errorf("Value A should be 7, instead it is %d", castedValue.A)
-	} else if castedValue.B != "qwer" {
-		t.Errorf("Value A should be qwer, instead it is %s", castedValue.B)
-	}
-	if _, err := q.Pop(); err == nil {
-		t.Errorf("Pop() on empty queue should return an error")
-	}
+	assert.Nil(t, q.Push(MyData{3, "asdf"}), "Pushing correct values should not return an error")
+	assert.Nil(t, q.Push(MyData{7, "qwer"}), "Pushing correct values should not return an error")
+	assert.Nil(t, q.Push(MyData{1, "zxcv"}), "Pushing correct values should not return an error")
+	assert.Equal(t, 3, q.Size(), "The queue should contain 3 elements")
+	value, err := q.Pop()
+	assert.Nil(t, err, "Error should be nil when poping elements from queue with elements")
+	assert.Equal(t, 1, value.(MyData).A, "Value A should be 1, instead it is %d", value.(MyData).A)
+	assert.Equal(t, "zxcv", value.(MyData).B, "Value A should be zxcv, instead it is %s", value.(MyData).B)
+	value, err = q.Pop()
+	assert.Nil(t, err, "Error should be nil when poping elements from queue with elements")
+	assert.Equal(t, 3, value.(MyData).A, "Value A should be 3, instead it is %d", value.(MyData).A)
+	assert.Equal(t, "asdf", value.(MyData).B, "Value A should be asdf, instead it is %s", value.(MyData).B)
+	value, err = q.Pop()
+	assert.Nil(t, err, "Error should be nil when poping elements from queue with elements")
+	assert.Equal(t, 7, value.(MyData).A, "Value A should be 7, instead it is %d", value.(MyData).A)
+	assert.Equal(t, "qwer", value.(MyData).B, "Value A should be qwer, instead it is %s", value.(MyData).B)
+	value, err = q.Pop()
+	assert.NotNil(t, err, "Pop() on empty queue should return an error")
 }
 
 func TestQueueWithWrongValues(t *testing.T) {
 	q := OrderedQueue{}
-	if err := q.Push(3); err != nil {
-		t.Errorf("Pushing correct values should not return an error")
-	}
-	if err := q.Push("asdf"); err == nil {
-		t.Errorf("Pushing wrong values should return an error")
-	}
-	if q.Size() != 1 {
-		t.Errorf("The queue should contain 1 element")
-	}
+	assert.Nil(t, q.Push(3), "Pushing correct values should not return an error")
+	assert.NotNil(t, q.Push("asdf"), "Pushing values of a different type from the original should return an error")
+	assert.Equal(t, 1, q.Size(), "The queue should contain 1 element")
 }
 
 func TestQueueWithMultipleTypes(t *testing.T) {
@@ -152,229 +97,145 @@ func TestQueueWithMultipleTypes(t *testing.T) {
 	{
 		vector := []int{1, 5, 3}
 		for _, element := range vector {
-			if err := q.Push(element); err != nil {
-				t.Errorf("Pushing correct values should not return an error")
-			}
+			assert.Nil(t, q.Push(element), "Pushing correct values should not return an error")
 		}
-		if q.Size() != 3 {
-			t.Errorf("The queue should contain 3 elements, instead int contains %d elements", q.Size())
-		}
+		assert.Equal(t, 3, q.Size(), "The queue should contain 3 elements, instead int contains %d elements", q.Size())
 		for _, _ = range vector {
-			if _, err := q.Pop(); err != nil {
-				t.Errorf("Pop() should not return an error")
-			}
+			_, err := q.Pop()
+			assert.Nil(t, err, "Pop() should not return an error")
 		}
-		if q.Size() != 0 {
-			t.Errorf("The queue should contain 0 elements")
-		}
+		assert.Equal(t, 0, q.Size(), "The queue should contain 0 elements")
 	}
 	{
 		vector := []int8{1, 5, 3}
 		for _, element := range vector {
-			if err := q.Push(element); err != nil {
-				t.Errorf("Pushing correct values should not return an error")
-			}
+			assert.Nil(t, q.Push(element), "Pushing correct values should not return an error")
 		}
-		if q.Size() != 3 {
-			t.Errorf("The queue should contain 3 elements, instead int contains %d elements", q.Size())
-		}
+		assert.Equal(t, 3, q.Size(), "The queue should contain 3 elements, instead int contains %d elements", q.Size())
 		for _, _ = range vector {
-			if _, err := q.Pop(); err != nil {
-				t.Errorf("Pop() should not return an error")
-			}
+			_, err := q.Pop()
+			assert.Nil(t, err, "Pop() should not return an error")
 		}
-		if q.Size() != 0 {
-			t.Errorf("The queue should contain 0 elements")
-		}
+		assert.Equal(t, 0, q.Size(), "The queue should contain 0 elements")
 	}
 	{
 		vector := []int16{1, 5, 3}
 		for _, element := range vector {
-			if err := q.Push(element); err != nil {
-				t.Errorf("Pushing correct values should not return an error")
-			}
+			assert.Nil(t, q.Push(element), "Pushing correct values should not return an error")
 		}
-		if q.Size() != 3 {
-			t.Errorf("The queue should contain 3 elements, instead int contains %d elements", q.Size())
-		}
+		assert.Equal(t, 3, q.Size(), "The queue should contain 3 elements, instead int contains %d elements", q.Size())
 		for _, _ = range vector {
-			if _, err := q.Pop(); err != nil {
-				t.Errorf("Pop() should not return an error")
-			}
+			_, err := q.Pop()
+			assert.Nil(t, err, "Pop() should not return an error")
 		}
-		if q.Size() != 0 {
-			t.Errorf("The queue should contain 0 elements")
-		}
+		assert.Equal(t, 0, q.Size(), "The queue should contain 0 elements")
 	}
 	{
 		vector := []int32{1, 5, 3}
 		for _, element := range vector {
-			if err := q.Push(element); err != nil {
-				t.Errorf("Pushing correct values should not return an error")
-			}
+			assert.Nil(t, q.Push(element), "Pushing correct values should not return an error")
 		}
-		if q.Size() != 3 {
-			t.Errorf("The queue should contain 3 elements, instead int contains %d elements", q.Size())
-		}
+		assert.Equal(t, 3, q.Size(), "The queue should contain 3 elements, instead int contains %d elements", q.Size())
 		for _, _ = range vector {
-			if _, err := q.Pop(); err != nil {
-				t.Errorf("Pop() should not return an error")
-			}
+			_, err := q.Pop()
+			assert.Nil(t, err, "Pop() should not return an error")
 		}
-		if q.Size() != 0 {
-			t.Errorf("The queue should contain 0 elements")
-		}
+		assert.Equal(t, 0, q.Size(), "The queue should contain 0 elements")
 	}
 	{
 		vector := []int64{1, 5, 3}
 		for _, element := range vector {
-			if err := q.Push(element); err != nil {
-				t.Errorf("Pushing correct values should not return an error")
-			}
+			assert.Nil(t, q.Push(element), "Pushing correct values should not return an error")
 		}
-		if q.Size() != 3 {
-			t.Errorf("The queue should contain 3 elements, instead int contains %d elements", q.Size())
-		}
+		assert.Equal(t, 3, q.Size(), "The queue should contain 3 elements, instead int contains %d elements", q.Size())
 		for _, _ = range vector {
-			if _, err := q.Pop(); err != nil {
-				t.Errorf("Pop() should not return an error")
-			}
+			_, err := q.Pop()
+			assert.Nil(t, err, "Pop() should not return an error")
 		}
-		if q.Size() != 0 {
-			t.Errorf("The queue should contain 0 elements")
-		}
+		assert.Equal(t, 0, q.Size(), "The queue should contain 0 elements")
 	}
 	{
 		vector := []uint{1, 5, 3}
 		for _, element := range vector {
-			if err := q.Push(element); err != nil {
-				t.Errorf("Pushing correct values should not return an error")
-			}
+			assert.Nil(t, q.Push(element), "Pushing correct values should not return an error")
 		}
-		if q.Size() != 3 {
-			t.Errorf("The queue should contain 3 elements, instead int contains %d elements", q.Size())
-		}
+		assert.Equal(t, 3, q.Size(), "The queue should contain 3 elements, instead int contains %d elements", q.Size())
 		for _, _ = range vector {
-			if _, err := q.Pop(); err != nil {
-				t.Errorf("Pop() should not return an error")
-			}
+			_, err := q.Pop()
+			assert.Nil(t, err, "Pop() should not return an error")
 		}
-		if q.Size() != 0 {
-			t.Errorf("The queue should contain 0 elements")
-		}
+		assert.Equal(t, 0, q.Size(), "The queue should contain 0 elements")
 	}
 	{
 		vector := []uint8{1, 5, 3}
 		for _, element := range vector {
-			if err := q.Push(element); err != nil {
-				t.Errorf("Pushing correct values should not return an error")
-			}
+			assert.Nil(t, q.Push(element), "Pushing correct values should not return an error")
 		}
-		if q.Size() != 3 {
-			t.Errorf("The queue should contain 3 elements, instead int contains %d elements", q.Size())
-		}
+		assert.Equal(t, 3, q.Size(), "The queue should contain 3 elements, instead int contains %d elements", q.Size())
 		for _, _ = range vector {
-			if _, err := q.Pop(); err != nil {
-				t.Errorf("Pop() should not return an error")
-			}
+			_, err := q.Pop()
+			assert.Nil(t, err, "Pop() should not return an error")
 		}
-		if q.Size() != 0 {
-			t.Errorf("The queue should contain 0 elements")
-		}
+		assert.Equal(t, 0, q.Size(), "The queue should contain 0 elements")
 	}
 	{
 		vector := []uint16{1, 5, 3}
 		for _, element := range vector {
-			if err := q.Push(element); err != nil {
-				t.Errorf("Pushing correct values should not return an error")
-			}
+			assert.Nil(t, q.Push(element), "Pushing correct values should not return an error")
 		}
-		if q.Size() != 3 {
-			t.Errorf("The queue should contain 3 elements, instead int contains %d elements", q.Size())
-		}
+		assert.Equal(t, 3, q.Size(), "The queue should contain 3 elements, instead int contains %d elements", q.Size())
 		for _, _ = range vector {
-			if _, err := q.Pop(); err != nil {
-				t.Errorf("Pop() should not return an error")
-			}
+			_, err := q.Pop()
+			assert.Nil(t, err, "Pop() should not return an error")
 		}
-		if q.Size() != 0 {
-			t.Errorf("The queue should contain 0 elements")
-		}
+		assert.Equal(t, 0, q.Size(), "The queue should contain 0 elements")
 	}
 	{
 		vector := []uint32{1, 5, 3}
 		for _, element := range vector {
-			if err := q.Push(element); err != nil {
-				t.Errorf("Pushing correct values should not return an error")
-			}
+			assert.Nil(t, q.Push(element), "Pushing correct values should not return an error")
 		}
-		if q.Size() != 3 {
-			t.Errorf("The queue should contain 3 elements, instead int contains %d elements", q.Size())
-		}
+		assert.Equal(t, 3, q.Size(), "The queue should contain 3 elements, instead int contains %d elements", q.Size())
 		for _, _ = range vector {
-			if _, err := q.Pop(); err != nil {
-				t.Errorf("Pop() should not return an error")
-			}
+			_, err := q.Pop()
+			assert.Nil(t, err, "Pop() should not return an error")
 		}
-		if q.Size() != 0 {
-			t.Errorf("The queue should contain 0 elements")
-		}
+		assert.Equal(t, 0, q.Size(), "The queue should contain 0 elements")
 	}
 	{
 		vector := []uint64{1, 5, 3}
 		for _, element := range vector {
-			if err := q.Push(element); err != nil {
-				t.Errorf("Pushing correct values should not return an error")
-			}
+			assert.Nil(t, q.Push(element), "Pushing correct values should not return an error")
 		}
-		if q.Size() != 3 {
-			t.Errorf("The queue should contain 3 elements, instead int contains %d elements", q.Size())
-		}
+		assert.Equal(t, 3, q.Size(), "The queue should contain 3 elements, instead int contains %d elements", q.Size())
 		for _, _ = range vector {
-			if _, err := q.Pop(); err != nil {
-				t.Errorf("Pop() should not return an error")
-			}
+			_, err := q.Pop()
+			assert.Nil(t, err, "Pop() should not return an error")
 		}
-		if q.Size() != 0 {
-			t.Errorf("The queue should contain 0 elements")
-		}
+		assert.Equal(t, 0, q.Size(), "The queue should contain 0 elements")
 	}
 	{
 		vector := []float32{1.0, 5.0, 3.0}
 		for _, element := range vector {
-			if err := q.Push(element); err != nil {
-				t.Errorf("Pushing correct values should not return an error")
-			}
+			assert.Nil(t, q.Push(element), "Pushing correct values should not return an error")
 		}
-		if q.Size() != 3 {
-			t.Errorf("The queue should contain 3 elements, instead int contains %d elements", q.Size())
-		}
+		assert.Equal(t, 3, q.Size(), "The queue should contain 3 elements, instead int contains %d elements", q.Size())
 		for _, _ = range vector {
-			if _, err := q.Pop(); err != nil {
-				t.Errorf("Pop() should not return an error")
-			}
+			_, err := q.Pop()
+			assert.Nil(t, err, "Pop() should not return an error")
 		}
-		if q.Size() != 0 {
-			t.Errorf("The queue should contain 0 elements")
-		}
+		assert.Equal(t, 0, q.Size(), "The queue should contain 0 elements")
 	}
 	{
 		vector := []float64{1.0, 5.0, 3.0}
 		for _, element := range vector {
-			if err := q.Push(element); err != nil {
-				t.Errorf("Pushing correct values should not return an error")
-			}
+			assert.Nil(t, q.Push(element), "Pushing correct values should not return an error")
 		}
-		if q.Size() != 3 {
-			t.Errorf("The queue should contain 3 elements, instead int contains %d elements", q.Size())
-		}
+		assert.Equal(t, 3, q.Size(), "The queue should contain 3 elements, instead int contains %d elements", q.Size())
 		for _, _ = range vector {
-			if _, err := q.Pop(); err != nil {
-				t.Errorf("Pop() should not return an error")
-			}
+			_, err := q.Pop()
+			assert.Nil(t, err, "Pop() should not return an error")
 		}
-		if q.Size() != 0 {
-			t.Errorf("The queue should contain 0 elements")
-		}
+		assert.Equal(t, 0, q.Size(), "The queue should contain 0 elements")
 	}
 }
